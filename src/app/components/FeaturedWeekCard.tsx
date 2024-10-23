e64f9062af7c03a10cce94ef0c9ced5aaf8c20c1
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-10-23 18:54:47 +0100

    feature:Updated FeaturedWeekCard and FeaturedWeekCateogry components with layout and styling changes, and added Image component from next/image.

--- src/app/components/FeaturedWeekCard.tsx
+++ src/app/components/FeaturedWeekCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import StarCount from "./StarCount";
 import Button from "./Button";
+import Image from "next/image";
 
 interface DataType{
     image: string;
@@ -13,12 +14,10 @@ interface DataType{
 export default function FeaturedWeekCard({image,Text,Price,Old_Price,starCount}:DataType) {
     const baseUrl = `/Products`;
   return (
-    <section className="mt-20 flex justify-between w-full gap-4">
-      <section className="flex w-[40%] bg-white p-4 items-center shadow-md rounded-sm">
+    <section className="mt-20 w-[40%] h-[35vh]">
+      <section className="flex w-full bg-white p-4 items-center shadow-md rounded-sm">
         <aside className="flex flex-col gap-4">
-          <h1 className="text-2xl font-semibold">
-            {Text}
-          </h1>
+          <h1 className="text-2xl font-semibold">{Text}</h1>
           <div className="flex gap-4">
             <p>{Price}</p>
             <del> {Old_Price}</del>
@@ -26,18 +25,19 @@ export default function FeaturedWeekCard({image,Text,Price,Old_Price,starCount}:
 
           <StarCount Count={starCount} />
 
-          <Button Width="60%" Link="" Text="Shop now" Pointer={true} />
+          <Button Width="70%" Link="" Text="Shop now" Pointer={true} />
         </aside>
 
-        <aside className="w-[70%]">
-          <img
+        <aside className="w-[70%]    h-[35vh] ">
+          <Image
             src={`${baseUrl}/${image}`}
             alt=""
+            width={100}
+            height={100}
             className="w-[200px]"
           />
         </aside>
       </section>
- 
     </section>
   );
 }
--- src/app/components/FeaturedWeekCateogry.tsx
+++ src/app/components/FeaturedWeekCateogry.tsx
@@ -4,14 +4,36 @@ import FeaturedWeekCard from './FeaturedWeekCard';
 
 export default function FeaturedWeekCateogry() {
   return (
-    <section className='bg-background py-20 px-10 mb-20'>
+    <section className="bg-background py-20 px-10 mb-20 w-full">
       <SectionTitleCard
         HeaderText="This Week’s Featured "
         Position="start"
         Text="Shop By Category "
       />
 
-      <FeaturedWeekCard />
+      <section className="flex w-full gap-5 justify-center items-center ">
+        <FeaturedWeekCard
+          Text="Vera Bradly Straw Tote Bags"
+          Price="$65.00"
+          Old_Price="$89.00"
+          image="prodcut-9.png"
+          starCount={5}
+        />
+        <FeaturedWeekCard
+          Text="Gucci Women’s Red Bag"
+          Price=" $75.00"
+          Old_Price="$90.00"
+          image="prodcut-10.png"
+          starCount={5}
+        />
+        <FeaturedWeekCard
+          Text="Men’s Jacket Premium Cotton "
+          Price="$65.00"
+          Old_Price="$79.00"
+          image="prodcut-8.png"
+          starCount={5}
+        />
+      </section>
     </section>
   );
 }
